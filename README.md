515a988532910eff584e9d411af8c828cdfad8e0
Ahmad Junjung Sudrajad <junjungahmad@gmail.com>
2022-10-24 10:28:12 +0700

    Deploy

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# TKC-CBIR-Deploy
+# TUGAS 3 & 4 TEMU KEMBALI CITRA
+
+##Anggota Kelompok :
+### Aris Muhandisin 201910370311432
+## membuat dan meggunakan model yang sesuai dengan dataset di paper referensi utama dan mendeploy program di heroku
+## Link Heroku/AWS : -
+## Link Jurnal Referensi : https://www.tandfonline.com/doi/abs/10.1080/02522667.2020.1809096?cookieSet=1
+## Link Dataset : https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia
+
+## Hasil Percobaan
+![image](https://user-images.githubusercontent.com/79823148/195783033-fead89fe-1370-4be5-b39f-a43e504d7c18.png)
+
+
+
+
+
---
+++ feature_extractor.py
@@ -0,0 +1,21 @@
+from tensorflow.keras.applications.densenet import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+
+class FeatureExtractor:
+    def __init__(self, model):
+        self.model = model
+
+    def extract(self, img):
+        img = img.resize((224, 224))
+        img = img.convert('RGB')
+#        img = np.reshape(img, (150, 150,3))
+
+        x = img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        
+        feature = self.model.predict(x)[0]
+        
+        return feature / np.linalg.norm(feature)
+
